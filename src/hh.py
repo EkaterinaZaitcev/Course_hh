aaa6e75d3f53e0243e46c2043e0c3928d3acd57a
Ekaterina <katyat1@mail.ru>
2024-11-21 16:19:05 +1000

    Прописан класс вакансии, работа с json.

--- src/hh.py
+++ src/hh.py
@@ -1,4 +1,15 @@
+from abc import ABC, abstractmethod
+import json
 import requests
+
+
+class Parser(ABC):
+    """Родительский класс get-запросов"""
+
+    @abstractmethod
+    def load_vacancies(self, keyword):
+        """Метод отправки get-запроса на сайт Head Hunter"""
+        pass
 
 
 class HH(Parser):
@@ -8,16 +19,23 @@ class HH(Parser):
     """
 
     def __init__(self, file_worker):
-        self.url = 'https://api.hh.ru/vacancies'
-        self.headers = {'User-Agent': 'HH-User-Agent'}
-        self.params = {'text': '', 'page': 0, 'per_page': 100}
-        self.vacancies = []
+        self.__url = 'https://api.hh.ru/vacancies'
+        self.__headers = {'User-Agent': 'HH-User-Agent'}
+        self.__params = {'text': '', 'page': 0, 'per_page': 100}
+        self.__vacancies = []
         super().__init__(file_worker)
 
+    def __api_connect(self):
+        """Подключение к API hh.ru"""
+        response = requests.get(self.__url, headers=self.__headers, params=self.__params)
+        if response.status_code == 200:
+            return response
+        print("Ошибка получения данных")
+
     def load_vacancies(self, keyword):
-        self.params['text'] = keyword
-        while self.params.get('page') != 20:
-            response = requests.get(self.url, headers=self.headers, params=self.params)
+        self.__params['text'] = keyword
+        while self.__params.get('page') != 20:
+            response = requests.get(self.__url, headers=self.__headers, params=self.__params)
             vacancies = response.json()['items']
-            self.vacancies.extend(vacancies)
-            self.params['page'] += 1
+            self.__vacancies.extend(vacancies)
+            self.__params['page'] += 1
